8deb449ff0ca67be871ead7cbd84c1c8f022403b
Denis Mustafaraj <denismustafaraj1@gmail.com>
2026-02-22 16:21:56 +0100

    added search bar and search button to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,30 @@ function Home(){
         {id: 3 , title: "John Wick" , releaseDate: "2000"},
     ]
 
+    const heandleSearch = () => {
 
-    return <div className="home">
-        <div className="movies-grid">
-            {movies.map((movie) => (
-                <MovieCard movie={movie} key={movie.id}/>
-            ))}
+    }
+
+    return (
+        <div className="home">
+
+            <form onSubmit={heandleSearch} className="search-form">
+                <input 
+                    type="text" 
+                    placeholder="Search for movie..." 
+                    className="search-input"
+                />
+
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id}/>
+                ))}
+            </div>
         </div>
-    </div>
+    );
 }
 
 export default Home
